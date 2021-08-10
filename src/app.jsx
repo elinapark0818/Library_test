fd38287b5062e899a9ecc3584f554113ed9366be
Elina <elinapark0818@gmail.com>
2021-08-10 23:22:01 +0900

    React Simple-slider Autoplay

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import styles from './app.module.css';
 import 'react-awesome-button/dist/themes/theme-blue.css';
 import Button from './components/awesome_btn/awesome_btn';
+import SimpleSlider from './components/simple_slider/simple_slider';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 function App() {
   return (
     <div className={styles.container}>
       <Button />
+      <SimpleSlider />
     </div>
     
   );
